896cb56a23f36c3de577b77cdc3e6d806b45aa7a
Max-Y-Ma <maxma2@illinois.edu>
2024-01-07 20:56:05 -0500

    Skeletons

--- linux/core/cereal.h
+++ linux/core/cereal.h
@@ -1,6 +1,30 @@
 #ifndef _CEREAL_H_
 #define _CEREAL_H_
 
+#include "stdint.h"
+
 #include <linux/types.h>
+
+#define TX_BUFFER_LENGTH    1024
+#define RX_BUFFER_LENGTH    1024
+
+/* Data Structures */
+typedef void (*cereal_callback)(uint8_t* buf);
+typedef struct cereal_t{
+    uint32_t fd;
+    uint8_t rx_buf[RX_BUFFER_LENGTH];
+    uint8_t tx_buf[TX_BUFFER_LENGTH];
+    uint8_t has_callback;
+    cereal_callback cb;
+} cereal_t;
+typedef cereal_t* cereal_handle_t;
+
+
+/* Function Prototypes */
+void cereal_init(cereal_handle_t hcereal);
+void cereal_connect(cereal_handle_t hcereal);
+void cereal_transmit(cereal_handle_t hcereal, uint8_t* tx_buf, uint32_t nbytes);
+void cereal_receive(cereal_handle_t hcereal, uint8_t* rx_buf, uint32_t nbytes);
+void cereal_register_callback(cereal_handle_t hcereal, cereal_callback cb);
 
 #endif /* _CEREAL_H_ */
--- linux/main.c
+++ linux/main.c
@@ -1,10 +1,37 @@
 /* Standard Includes */
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 /* Library Includes */
 #include "core/cereal.h"
 
+/* User callback */
+static void myhandler(uint8_t* buf);
+
 int main() {
-    printf("Hello World!\n");
+    /* Instantiate and initialize handler */
+    cereal_handle_t hcereal1 = (cereal_handle_t) malloc(sizeof(cereal_t));
+    cereal_init(hcereal1);
+    cereal_register_callback(hcereal1, (cereal_callback)myhandler);
+
+    /* Connect to serial device */
+    cereal_connect(hcereal1);
+
+    /* Run test */
+    cereal_transmit(hcereal1, "Hello!", strlen("Hello!"));
+
+    uint8_t buffer[RX_BUFFER_LENGTH];
+    cereal_receive(hcereal1, buffer, RX_BUFFER_LENGTH);
+
+    if (strcmp(buffer, "ACK") != 0) {
+        return -1;
+    }
+
     return 0;
+}
+
+static void myhandler(uint8_t* buf)
+{
+    /* Process data in buffer */
 }
